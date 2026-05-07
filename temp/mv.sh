7831d22dcfb74bb16cd00645c386692ed0af0d40
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-07 16:18:00 +0000

    temp script to move data

---
+++ temp/mv.sh
@@ -0,0 +1,3 @@
+# bash script to move file to new folder
+# mc mv s3/projet-funathon/2026/project1/transactions_flats.parquet s3/projet-funathon/2026/project1/data/raw/transactions_flats_FR_raw.parquet
+# mc mv s3/projet-funathon/2026/project1/transactions_houses.parquet s3/projet-funathon/2026/project1/data/raw/transactions_houses_FR_raw.parquet
