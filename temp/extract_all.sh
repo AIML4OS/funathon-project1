7ad4622a71015d02b3b070a7fcc14e6adc16be7a
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-11 17:15:30 +0000

    with mkdir if dir doesnt exist

--- temp/extract_all.sh
+++ temp/extract_all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 FILE_1="intermediate_solutions/solution_step1/main.py"
+mkdir -p $(dirname "$FILE_1")
 FILE_2A="intermediate_solutions/solution_step1_to_2a/main.py"
+mkdir -p $(dirname "$FILE_2A")
 FILE_2B="intermediate_solutions/solution_step1_to_2b/main.py"
+mkdir -p $(dirname "$FILE_2B")
 
 # Step 1 - preprocessing
 bash temp/extract.sh "1-preprocessing.qmd" $FILE_1
