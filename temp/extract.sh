eaab3231c3f340ba891fa0132f7152c9d4f8d705
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-11 12:22:55 +0000

    update script to create scripts

--- temp/extract.sh
+++ temp/extract.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+# bash temp/extract.sh "2-GB_model.qmd" "intermediate_solutions/solution_step1_to_2a/main.py"
+# bash temp/extract.sh "2-RF_model.qmd" "intermediate_solutions/solution_step1_to_2b/main.py"
+# bash temp/extract.sh "1-preprocessing.qmd" "intermediate_solutions/solution_step1/main.py"
 
 # Check if input file is provided
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <input_file.qmd>"
     exit 1
 fi
 
 input_file="$1"
-output_file="${input_file%.qmd}.py"
+
+if [ "$#" -ne 2 ]; then
+    output_file="${input_file%.qmd}.py"
+else
+    output_file="$2"
+    folder_output=$(dirname "$output_file")
+    mkdir -p "$folder_output" && echo "Folder '$folder_output' created."
+fi
+
+
 
 # Extract Python code blocks, remove #| lines, and trim the exact number of leading whitespace
 awk '
@@ -34,4 +46,4 @@ flag {
 }
 ' "$input_file" > "$output_file"
 
-echo "Python code blocks extracted to $output_file (leading whitespace removed consistently)"
+echo "Python code blocks extracted from $input_file ==> $output_file"
