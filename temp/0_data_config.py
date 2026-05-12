8b2182cd91b4f14e398e0ee03f3ef160d2d0e0a4
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-12 09:58:19 +0000

    rename folders

--- temp/0_data_config.py
+++ temp/0_data_config.py
@@ -18,7 +18,7 @@ def data_fr_raw_to_en(file_names_list, mapping, file_out_name):
     cols_to_keep = list(mapping_fr_en.keys())
 
     for file_name in file_names_list:
-        FILE_PATH_S3 = BUCKET + "/" + "2026/project1/data/raw/" + file_name
+        FILE_PATH_S3 = BUCKET + "/" + "2026/project1/data/0_raw/" + file_name
 
         with fs.open(FILE_PATH_S3, mode="rb") as file_in:
             df_raw = pd.read_parquet(file_in)
@@ -28,7 +28,7 @@ def data_fr_raw_to_en(file_names_list, mapping, file_out_name):
     df_en = pd.concat(list_df)
     df_en = df_en.rename(columns=mapping)
 
-    FILE_PATH_OUT_S3 = "s3://" + BUCKET + "/2026/project1/data/" + file_out_name
+    FILE_PATH_OUT_S3 = "s3://" + BUCKET + "/2026/project1/data/1_input" + file_out_name
 
     df_en.to_parquet(FILE_PATH_OUT_S3, index=False)
 
