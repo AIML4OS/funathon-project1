3ca54cf511a13ef39a5f45321f740a710dfb7634
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-13 15:34:44 +0000

    mv to solution

--- temp/mv.sh
+++
@@ -1,3 +0,0 @@
-# bash script to move file to new folder
-# mc mv s3/projet-funathon/2026/project1/transactions_flats.parquet s3/projet-funathon/2026/project1/data/raw/transactions_flats_FR_raw.parquet
-# mc mv s3/projet-funathon/2026/project1/transactions_houses.parquet s3/projet-funathon/2026/project1/data/raw/transactions_houses_FR_raw.parquet
