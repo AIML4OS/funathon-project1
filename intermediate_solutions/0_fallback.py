6c9b6142d1953804c0bf727419fabc4c1904dd0d
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-21 15:33:12 +0000

    typo fallback

--- intermediate_solutions/0_fallback.py
+++ intermediate_solutions/0_fallback.py
@@ -2,7 +2,7 @@
 #  %%
 import sys
 sys.path.append("..")
-sys.path.append("../solution/pipeline.py")
+sys.path.append("../solution")
 import pandas as pd
 from joblib import load
 from solution.utils import set_seed, set_s3fs, generate_file_path_s3_models
@@ -10,8 +10,11 @@ from solution.pipeline import set_date_transformer, set_preprocessor, set_y_tran
 
 RANDOM_STATE = set_seed()
 
-df_path_back_up = "https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/df.parquet"
-df = pd.read_parquet(df_path_back_up)
+df      = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/df.parquet")
+X_train = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/X_train.parquet")
+X_test  = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/X_test.parquet")
+y_train = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/y_train.parquet")["price_sqm"]
+y_test  = pd.read_parquet("https://minio.lab.sspcloud.fr/projet-funathon/2026/project1/data/2_preprocessing/y_test.parquet")["price_sqm"]
 
 # Create filesystem object
 fs = set_s3fs()
