a847269d724d36a72230c66651207f8c622869ff
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-21 10:35:33 +0000

    mv to utils

--- solution/utils.py
+++ solution/utils.py
@@ -1,5 +1,10 @@
 import logging
 import datetime
+import pandas as pd
+import mlflow.sklearn
+import os
+import s3fs
+from joblib import dump
 
 
 def setup_logging():
@@ -21,8 +26,9 @@ def set_seed():
     return 202605
 
 
+# %%
 def check_data(df):
-    dataframe_name = [name for name, val in globals().items() if val is df][0]
+    dataframe_name = [name for name, val in globals().items() if val is df]
 
     res_dict = {
         "dataframe_name": dataframe_name,
@@ -32,10 +38,10 @@ def check_data(df):
     }
 
     msg = f"""\
-= {dataframe_name} ========= 
+= {dataframe_name} =========
 hash              : {res_dict["hash"]}
-number of columns : {res_dict["n_cols"]} 
-number of rows    : {res_dict["n_rows"]} 
+number of columns : {res_dict["n_cols"]}
+number of rows    : {res_dict["n_rows"]}
 
 """
 
@@ -48,17 +54,63 @@ number of rows    : {res_dict["n_rows"]}
 # print(check_data(X_train)["msg"])
 # print(check_data(X_test)["msg"])
 
-# = df ========= 
+# = df =========
 # hash              : 17566835255591554045
-# number of columns : 40 
-# number of rows    : 1694285 
+# number of columns : 40
+# number of rows    : 1694285
 
-# = X_train ========= 
+# = X_train =========
 # hash              : 9740245480247122702
-# number of columns : 39 
-# number of rows    : 1355428 
+# number of columns : 39
+# number of rows    : 1355428
 
-# = X_test ========= 
+# = X_test =========
 # hash              : 15218863447364480506
-# number of columns : 39 
-# number of rows    : 338857 
+# number of columns : 39
+# number of rows    : 338857
+
+
+# %%
+def set_s3fs():
+    # ── Setting the S3 connection and path generators ──────────
+    S3_ENDPOINT_URL = "https://" + os.environ["AWS_S3_ENDPOINT"]
+    fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
+
+    return fs
+
+
+def generate_file_path_s3(FILE_KEY_OUT_S3: str):
+    BUCKET_OUT = "projet-funathon"
+    return BUCKET_OUT + "/2026/project1/" + FILE_KEY_OUT_S3
+
+
+def generate_file_path_s3_models(FILE_KEY_OUT_S3: str):
+    return generate_file_path_s3(f"models/{FILE_KEY_OUT_S3}")
+
+
+def generate_file_path_s3_data(FILE_KEY_OUT_S3: str):
+    return generate_file_path_s3(f"data/{FILE_KEY_OUT_S3}")
+
+
+def store_datasets(datasets_to_store: dict):
+    # ── Storing datasets ────────────────────────────────────
+    # datasets_to_store = {"X_train": X_train, "X_test": X_test, "y_train": y_train.to_frame(), "y_test": y_test.to_frame(), "df": df}
+    fs = set_s3fs()
+    for name, data in datasets_to_store.items():
+        with fs.open(generate_file_path_s3_data(f"2_preprocessing/{name}.parquet"), 'wb') as file_out:
+            data.to_parquet(file_out, index=True)
+
+
+def store_model_mlflow_s3(model_uri: str, s3_name: str):
+    # ── Load GB from MLFlow ────────────────────────────────────
+    # model_uri = "models:/GB@latest"
+    # s3_name = "gb_model_final.joblib"
+    fs = set_s3fs()
+    model = mlflow.sklearn.load_model(model_uri)
+    # ── Storing GB model to S3 ─────────────────────────────────
+    with fs.open(generate_file_path_s3_models(s3_name), 'wb') as file_out:
+        dump(model, file_out)
+
+
+# store_model_mlflow_s3("models:/GB@latest", "gb_model_final.joblib")
+# store_model_mlflow_s3("models:/RF@latest", "rf_model_final.joblib")
