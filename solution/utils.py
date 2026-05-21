68673ac436fd89b3e722eff4e3a169bb4aa948d9
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-05-21 10:03:08 +0000

    mv check data to utils

--- solution/utils.py
+++ solution/utils.py
@@ -19,3 +19,46 @@ def setup_logging():
 
 def set_seed():
     return 202605
+
+
+def check_data(df):
+    dataframe_name = [name for name, val in globals().items() if val is df][0]
+
+    res_dict = {
+        "dataframe_name": dataframe_name,
+        "hash": int(pd.util.hash_pandas_object(df).sum()),
+        "n_cols": df.shape[1],
+        "n_rows": df.shape[0]
+    }
+
+    msg = f"""\
+= {dataframe_name} ========= 
+hash              : {res_dict["hash"]}
+number of columns : {res_dict["n_cols"]} 
+number of rows    : {res_dict["n_rows"]} 
+
+"""
+
+    res_dict["msg"] = msg
+
+    return res_dict
+
+
+# print(check_data(df)["msg"])
+# print(check_data(X_train)["msg"])
+# print(check_data(X_test)["msg"])
+
+# = df ========= 
+# hash              : 17566835255591554045
+# number of columns : 40 
+# number of rows    : 1694285 
+
+# = X_train ========= 
+# hash              : 9740245480247122702
+# number of columns : 39 
+# number of rows    : 1355428 
+
+# = X_test ========= 
+# hash              : 15218863447364480506
+# number of columns : 39 
+# number of rows    : 338857 
